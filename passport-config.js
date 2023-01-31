fabaae9aaab4d92eb62c59d4bd97d88b7d739581
Ryan Massey <rmassey.95@gmail.com>
2023-01-31 11:42:43 -0500

    Update passprot config

--- passport-config.js
+++ passport-config.js
@@ -7,7 +7,8 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:5000/odinbook/facebook/redirect",
+      callbackURL:
+        "https://odinbook-backend-c33h.onrender.com/odinbook/facebook/redirect",
       profileFields: ["id", "displayName", "photos", "email"],
     },
     function (accessToken, refreshToken, profile, cb) {
