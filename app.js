84764b9b92ce3128127c5126c195694114ddb60f
Ryan Massey <rmassey.95@gmail.com>
2023-02-11 11:25:03 -0500

    Add cors options

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const express = require("express");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-// const compression = require("compression");
+const compression = require("compression");
 const helmet = require("helmet");
 const port = process.env.PORT || 3001;
 const MongoStore = require("connect-mongo");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const odinbookRouter = require("./routes/odinbookRouter");
 
@@ -42,22 +43,31 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(compression()); // Compress all routes
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  // allow CORS for React App
-  res.setHeader("Access-Control-Allow-Origin", process.env.DOMAIN_URL);
-  // allow crendentials to be sent
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  // allow header to be set in React App
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  // allowed headers in requests
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  next();
-});
+app.use(
+  cors({
+    origin: "https://earnest-monstera-6c3cb0.netlify.app",
+    methods: "GET,PUT,POST,DELETE",
+    allowedHeaders: "Content-Type",
+    credentials: true,
+  })
+);
+
+// app.use((req, res, next) => {
+//   // allow CORS for React App
+//   res.setHeader("Access-Control-Allow-Origin", process.env.DOMAIN_URL);
+//   // allow crendentials to be sent
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   // allow header to be set in React App
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   // allowed headers in requests
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   next();
+// });
 
 app.use("/odinbook", odinbookRouter);
 
